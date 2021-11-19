954bb4c182f7f89c545ced9f557a47959ffce52e
gcleroux <guicleroux@icloud.com>
2021-11-19 13:59:06 -0500

    Code refactoring to encapsulate everything in fonctions

--- scratch.jl
+++ scratch.jl
@@ -1,5 +1,7 @@
 ### Scratch file to create a simple model to analyze Iris dataset
 
+##
+using Base: @kwdef
 using CUDA
 
 using Flux
@@ -8,7 +10,9 @@ using Flux.Data: DataLoader
 
 using MLDatasets
 using MLDataUtils
+##
 
+##
 function get_data(args)
 
     ENV["DATADEPS_ALWAYS_ACCEPT"] = true
@@ -34,26 +38,30 @@ function get_data(args)
     formatted_labels = onehotbatch(formatted_labels, 0:2)
 
     # Separating the datasets in training/testing
-    train_data, test_data = stratifiedobs((data, formatted_labels), p=0.8)
+    train_data, test_data = stratifiedobs((data, formatted_labels), p = 0.8)
 
-    train_loader, test_loader = DataLoader(train_data, batchsize=args.bs, shuffle=true), DataLoader(test_data, batchsize=args.bs)
+    train_loader, test_loader = DataLoader(train_data, batchsize = args.bs, shuffle = true), DataLoader(test_data, batchsize = args.bs)
 
     return train_loader, test_loader
 
 end
+##
 
+##
 function create_model()
-    
+
     # Creating a simple linear model
     model = Flux.Chain(
-        Flux.Dense(4, 16, relu),
-        Flux.Dense(16, 42, relu),
+        Flux.Dense(4, 24, relu),
+        Flux.Dense(24, 42, relu),
         Flux.Dense(42, 3)
     )
 
     return model
 end
+##
 
+##
 function get_device(args)
     # Choosing the device
     if CUDA.functional() && args.use_cuda
@@ -64,59 +72,69 @@ function get_device(args)
         return cpu
     end
 end
+##
 
+##
+function train(; kws...)
 
-struct args
-    lr::Float64
-    bs::Int
-    epochs::Int
-    use_cuda::Bool
-end
+    hp = Args(kws...)
 
-hp = args(1e-2, 25, 25, true)
+    train_loader, test_loader = get_data(hp)
 
-train_loader, test_loader = get_data(hp)
+    device = get_device(hp)
 
-device = get_device(hp)
+    model = create_model() |> device
 
-model = create_model() |> device
+    ## Training the model
+    ps = Flux.params(model)
 
-## Training the model
-ps = Flux.params(model)
+    loss = Flux.Losses.logitcrossentropy
 
-loss = Flux.Losses.logitcrossentropy
+    optimizer = Flux.ADAM(hp.lr)
 
-optimizer = Flux.ADAM(hp.lr)
+    for epoch = 1:hp.epochs
+        for (index, (X, y)) in enumerate(train_loader)
+            X, y = device(X), device(y)
 
-for epoch in 1:hp.epochs
-    for (index, (X, y)) in enumerate(train_loader)
-        X, y = device(X), device(y)
-        
-        l = nothing
-        gs = Flux.gradient(ps) do
-            ŷ = model(X)
-            l = loss(ŷ, y)
+            l = nothing
+            gs = Flux.gradient(ps) do
+                ŷ = model(X)
+                l = loss(ŷ, y)
+            end
+
+            index == length(train_loader) ? println("Epoch: ", epoch, ", Loss: ", l) : nothing
+
+            Flux.update!(optimizer, ps, gs)
         end
-        
-        index == length(train_loader) ? println("Epoch: ", epoch, ", Loss: ", l) : nothing
-
-        Flux.update!(optimizer, ps, gs)
     end
+
+    ## Testing the model
+    acc = 0
+    n = 0
+
+    for (X, y) in test_loader
+
+        X, y = device(X), device(y)
+
+        ŷ = model(X)
+
+        acc += sum(onecold(ŷ) .== onecold(y))
+
+        n += size(X)[2]
+    end
+
+    println("\n\nAccuracy: ", acc / n)
+end
+##
+
+
+# Defining the hyperparameters struct
+@kwdef mutable struct Args
+    lr::Float64 = 3e-3
+    bs::Int = 25
+    epochs::Int = 25
+    use_cuda::Bool = true
 end
 
-## Testing the model
-acc = 0
-n = 0
 
-for (X, y) in test_loader
-
-    X, y = device(X), device(y)
-
-    ŷ = model(X)
-
-    global acc += sum(onecold(ŷ) .== onecold(y))
-
-    global n += size(X)[2]
-end
-
-println("\n\nAccuracy: ", acc/n)
+train()
